70c85960e40d39dd8d2776c1562656d93f952e22
Renato Fagalde <renato@likwi.com.br>
2025-08-21 21:17:17 -0300

    :sparkles: feat: login page

--- webapp/cmd/web/routes.go
+++ webapp/cmd/web/routes.go
@@ -13,6 +13,7 @@ func (app *application) routes() http.Handler {
 	mux.Use(app.addIPToContext)
 
 	mux.Get("/", app.Home)
+	mux.Post("/login", app.Login)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
