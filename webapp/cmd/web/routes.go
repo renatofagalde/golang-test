bc736aca26802fe3fcdf71408a97620e55911616
Renato Fagalde <renato@likwi.com.br>
2025-08-23 15:39:42 -0300

    :recycle: refactor routes to use session manager

--- webapp/cmd/web/routes.go
+++ webapp/cmd/web/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(app.addIPToContext)
+	mux.Use(app.Session.LoadAndSave)
 
 	mux.Get("/", app.Home)
 	mux.Post("/login", app.Login)
