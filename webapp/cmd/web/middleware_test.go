41fc9b02ca21a9aee6f7760337d22cf8e1c53694
Renato Fagalde <renato@likwi.com.br>
2025-09-07 21:38:29 -0300

    :test_tube: test: create auth test

--- webapp/cmd/web/middleware_test.go
+++ webapp/cmd/web/middleware_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bootstrap/pkg/data"
 	"context"
 	"net/http"
 	"net/http/httptest"
@@ -66,4 +67,40 @@ func Test_application_ipFromContext(t *testing.T) {
 	if !strings.EqualFold("xpto", ip) {
 		t.Error("wrong value returned from context")
 	}
+}
+
+func Test_app_auth(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	var tests = []struct {
+		name   string
+		isAuth bool
+	}{
+		{"logged in", true},
+		{"not logged in", false},
+	}
+
+	for _, e := range tests {
+		handlerToTest := app.auth(nextHandler)
+		request := httptest.NewRequest("GET", "http://testing", nil)
+
+		request = addContextSessionToRequest(request, &app)
+		if e.isAuth {
+			app.Session.Put(request.Context(), "user", data.User{ID: 1})
+		}
+
+		rr := httptest.NewRecorder()
+		handlerToTest.ServeHTTP(rr, request)
+
+		if e.isAuth && rr.Code != http.StatusOK {
+			t.Errorf("%s: expected status code of 200 but got %d", e.name, rr.Code)
+		}
+
+		if !e.isAuth && rr.Code != http.StatusTemporaryRedirect {
+			t.Errorf("%s: expected status code 307, but got %d", e.name, rr.Code)
+		}
+	}
+
 }
