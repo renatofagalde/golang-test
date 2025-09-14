21ea55d67f30c0a7baa6ff92f02a23d0aa312c49
Renato Fagalde <renato@likwi.com.br>
2025-09-14 19:14:00 -0300

    :sparkles: feat: docker opt

--- webapp/pkg/repository/dbrepo/user_postgres_test.go
+++ webapp/pkg/repository/dbrepo/user_postgres_test.go
@@ -2,6 +2,7 @@ package dbrepo
 
 import (
 	"database/sql"
+	"log"
 	"os"
 	"testing"
 
@@ -9,6 +10,7 @@ import (
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 )
 
 var (
@@ -25,6 +27,38 @@ var pool *dockertest.Pool
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	// connect to dokcer. fail if docker not running
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatal("could not connect to docker; is it running? %s", err)
+	}
+
+	pool = p
+
+	// set up our docker options, specifying the image and so forth
+	opt := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "14,5",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWOR=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+
+	//get a resource - docker image
+
+	//start the image and wait until it's ready
+
+	//populate the database with empty tables
+
+	//run tests
 	code := m.Run()
 
 	os.Exit(code)
