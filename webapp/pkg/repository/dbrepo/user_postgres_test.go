98b1f1042c22db2cb0651e50d835863de791e9cc
Renato Fagalde <renato@likwi.com.br>
2025-09-14 18:37:39 -0300

    :sparkles: feat: new code for connection db in tests

---
+++ webapp/pkg/repository/dbrepo/user_postgres_test.go
@@ -0,0 +1,31 @@
+package dbrepo
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/ory/dockertest/v3"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "pass"
+	dbName   = "users_test"
+	port     = "5400"
+	dns      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezon=UTC connect_timeout=5"
+)
+
+var resource *dockertest.Resource
+var pool *dockertest.Pool
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	os.Exit(code)
+}
