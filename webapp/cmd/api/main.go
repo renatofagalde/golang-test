123de1883eeb71201d6e4d1552d8014e8a625773
Renato Fagalde <renato@likwi.com.br>
2025-11-20 17:38:06 -0300

    :sparkles: feat: add main application entry point with database connection

---
+++ webapp/cmd/api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bootstrap/pkg/repository"
+	"bootstrap/pkg/repository/dbrepo"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8090
+
+type application struct {
+	DSN       string
+	DB        repository.DatabaseRepository
+	Domain    string
+	JWTSecret string
+}
+
+func main() {
+	var app application
+	flag.StringVar(&app.Domain, "domain", "example.com", "Domain for application eg company.com")
+	flag.StringVar(&app.DSN, "dsn",
+		"host=localhost port=5490 user=postgres password=postgres dbname=users sslmode=disable"+
+			" timezone=UTC connect_timeout=5", "Postgres connection")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "chave-criptografia", "signing secret")
+
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+
+	app.DB.AllUsers()
+
+	//print out a message
+	log.Printf("Starting api on port %d\n", port)
+
+	//start the server
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
