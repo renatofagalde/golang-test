9e3680a7e7856bc532c0488e771ed49c70c8c414
Renato Fagalde <renato@likwi.com.br>
2025-11-22 17:11:45 -0300

    :sparkles: feat: define token structures and claims for JWT handling

--- webapp/cmd/api/auth.go
+++ webapp/cmd/api/auth.go
@@ -1,6 +1,20 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
 
 const jwtTokenExpiry = time.Minute * 15
 const refreshTokenExpiry = time.Hour * 24
+
+type TokenPairs struct {
+	Token        string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type Claims struct {
+	UserName string `json:"user_name"`
+	jwt.RegisteredClaims
+}
